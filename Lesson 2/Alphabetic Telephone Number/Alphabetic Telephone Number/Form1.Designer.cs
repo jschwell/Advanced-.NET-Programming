141e8a09d8438e443f0a53951337ca1a7cf0cea1
Joleen Schwellenbach <schwellenbachjc@my.witc.edu>
2016-04-13 20:10:02 -0500

    Added files via upload

---
+++ Lesson 2/Alphabetic Telephone Number/Alphabetic Telephone Number/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Alphabetic_Telephone_Number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grpTranslate = new System.Windows.Forms.GroupBox();
+            this.grpTranslated = new System.Windows.Forms.GroupBox();
+            this.lblTranslated = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.grpTranslate.SuspendLayout();
+            this.grpTranslated.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(55, 45);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(176, 20);
+            this.txtPhoneNumber.TabIndex = 0;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(184, 124);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+            this.btnTranslate.TabIndex = 1;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(346, 124);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // grpTranslate
+            // 
+            this.grpTranslate.Controls.Add(this.txtPhoneNumber);
+            this.grpTranslate.Location = new System.Drawing.Point(12, 12);
+            this.grpTranslate.Name = "grpTranslate";
+            this.grpTranslate.Size = new System.Drawing.Size(289, 100);
+            this.grpTranslate.TabIndex = 0;
+            this.grpTranslate.TabStop = false;
+            this.grpTranslate.Text = "Enter a phone number in the format XXX-XXX-XXXX";
+            // 
+            // grpTranslated
+            // 
+            this.grpTranslated.Controls.Add(this.lblTranslated);
+            this.grpTranslated.Location = new System.Drawing.Point(307, 12);
+            this.grpTranslated.Name = "grpTranslated";
+            this.grpTranslated.Size = new System.Drawing.Size(289, 100);
+            this.grpTranslated.TabIndex = 4;
+            this.grpTranslated.TabStop = false;
+            this.grpTranslated.Text = "Translated phone number";
+            // 
+            // lblTranslated
+            // 
+            this.lblTranslated.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTranslated.Location = new System.Drawing.Point(58, 44);
+            this.lblTranslated.Name = "lblTranslated";
+            this.lblTranslated.Size = new System.Drawing.Size(176, 20);
+            this.lblTranslated.TabIndex = 0;
+            this.lblTranslated.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Location = new System.Drawing.Point(265, 124);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnTranslate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(609, 159);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.grpTranslated);
+            this.Controls.Add(this.grpTranslate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "Form1";
+            this.Text = "Alphabetic Telephone Number Translator";
+            this.grpTranslate.ResumeLayout(false);
+            this.grpTranslate.PerformLayout();
+            this.grpTranslated.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox grpTranslate;
+        private System.Windows.Forms.GroupBox grpTranslated;
+        private System.Windows.Forms.Label lblTranslated;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Lesson 2/Alphabetic Telephone Number/Alphabetic Telephone Number/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alphabetic_Telephone_Number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValidFormat(string str)
+        {
+            const int VALID_LENGTH = 12;
+            bool valid;
+
+            // Determine whether str is properly formatted
+            if (str.Length == VALID_LENGTH && str[3] == '-' &&
+                str[7] == '-')
+            {
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+            // Return the value of valid
+            return valid;
+        }
+
+        private void Translate(ref string str)
+        {
+            for (int index = 0; index < str.Length; index++)
+            {
+                // If the character is a letter, translate it
+                if (char.IsLetter(str[index]))
+                {
+                    // Save character to a variable and remove the character in the string
+                    char letter = str[index];
+                    str = str.Remove(index, 1);
+
+                    // Insert the corresponding number in the string
+                    switch (char.ToUpper(letter))
+                    {
+                        case 'A':
+                        case 'B':
+                        case 'C':
+                            str = str.Insert(index, "2");
+                            break;
+                        case 'D':
+                        case 'E':
+                        case 'F':
+                            str = str.Insert(index, "3");
+                            break;
+                        case 'G':
+                        case 'H':
+                        case 'I':
+                            str = str.Insert(index, "4");
+                            break;
+                        case 'J':
+                        case 'K':
+                        case 'L':
+                            str = str.Insert(index, "5");
+                            break;
+                        case 'M':
+                        case 'N':
+                        case 'O':
+                            str = str.Insert(index, "6");
+                            break;
+                        case 'P':
+                        case 'Q':
+                        case 'R':
+                        case 'S':
+                            str = str.Insert(index, "7");
+                            break;
+                        case 'T':
+                        case 'U':
+                        case 'V':
+                            str = str.Insert(index, "8");
+                            break;
+                        case 'W':
+                        case 'X':
+                        case 'Y':
+                        case 'Z':
+                            str = str.Insert(index, "9");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            // Get a trimmed copy of the user's input
+            string input = txtPhoneNumber.Text.Trim();
+
+            // If the input is properly formatted, unformat it and display it
+            if (IsValidFormat(input))
+            {
+                Translate(ref input);
+                lblTranslated.Text = input;
+            }
+            else
+            {
+                // Display an error message
+                MessageBox.Show("Invalid input.");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear textbox and label
+            txtPhoneNumber.Text = "";
+            lblTranslated.Text = "";
+
+            // Set focus back to textbox
+            txtPhoneNumber.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
